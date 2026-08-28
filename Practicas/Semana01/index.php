034597363e675adb2042be1410cd06422bda27a5
Pedro Compean Orozco <pedrocompeanorozco@gmail.com>
2026-08-28 03:13:32 -0600

    creacion de HTML y primer variable de texto

---
+++ Practicas/Semana01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema de Inventario</title>
+</head>
+<body>
+    <h1>Gestor de Inventario</h1>
+    
+    <?php
+        // 1. Un dato de texto (Nombre del artículo en el almacén)
+        $articulo = "Monitor Dell 24 pulgadas";
+    ?>
+
+    <p><strong>Producto:</strong> <?php echo $articulo; ?></p>
+
+</body>
+</html>
